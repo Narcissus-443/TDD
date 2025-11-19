e5eef084ae1ce382ef34a3f565ba055f9b5ed2ea
Narcissus <244650596+Narcissus-443@users.noreply.github.com>
2025-11-19 11:12:27 +0100

    Update tdd.sh
    Bug fixes

--- tdd.sh
+++ tdd.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-VERSION=”7.2”
+VERSION="7.2"
 
-sudo mount -t tmpfs -o size=3G tmpfs “$HOME/Downloads”
-sudo chown -R “$(whoami)” “$HOME/Downloads”
-chmod -R 700 “$HOME/Downloads”
-wget –continue -O “$HOME/Downloads/tails-amd64-$VERSION” “https://download.tails.net/tails/stable/tails-amd64-$VERSION/tails-amd64-$VERSION.img”
-read -n 1 -s -p “Press any key to wipe Tails image file from this device”
-sudo umount “$HOME/Downloads”
+sudo mount -t tmpfs -o size=3G tmpfs "$HOME/Downloads"
+sudo chown -R "$(whoami)" "$HOME/Downloads"
+chmod -R 700 "$HOME/Downloads"
+cd "$HOME/Downloads"
+wget –continue "https://download.tails.net/tails/stable/tails-amd64-$VERSION/tails-amd64-$VERSION.img"
+read -n 1 -s -p "Press any key to wipe Tails image file from this device"
+rm -f "tails-amd64-$VERSION.img"
+cd "$HOME"
+sudo umount "$HOME/Downloads"
